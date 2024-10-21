aa2dce5fba294f03c34baa19791352d6221cb947
Ximena <Ximena.aleja021@outlook.com>
2024-10-21 13:25:07 -0500

    se agrega variable para calcular el monto del descuento

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 let precio = parseInt(prompt ("ingrese el precio de la prenda de vestir"))
 let descuento = parseInt(prompt ("ingrese el porcentaje de descuento"))
+
+let montodescuento = (precio*descuento)/100

08be6df7a8b0c2919a132180c93e5fcb32a66639
Ximena <Ximena.aleja021@outlook.com>
2024-10-21 13:25:47 -0500

    se agrega variable para saber precio final de la prenda con descuento

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ let precio = parseInt(prompt ("ingrese el precio de la prenda de vestir"))
 let descuento = parseInt(prompt ("ingrese el porcentaje de descuento"))
 
 let montodescuento = (precio*descuento)/100
+let preciofinal = precio-montodescuento
